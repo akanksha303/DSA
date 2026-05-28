7145a9363201bec79a25c9d91f19b1ab551a1134
Akanksha303 <tripathiakanksha339@gmail.com>
2026-05-28 11:37:16 +0530

    ADD MAX

---
+++ slidingwindow/maxsubarray.java
@@ -0,0 +1,17 @@
+public class maxsubarray {
+
+    public static int maxsubarray( int[] arr, int k ){
+        
+        int i ;
+        int j;
+        int sum;
+        int max;
+
+        while(j<length.arr()){
+            
+        }
+
+
+    }
+    
+}
